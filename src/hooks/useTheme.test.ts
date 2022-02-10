f1b809769ec0f80308bb1f65c12ac81ad0a67c5c
Lucas Viana <contato@lucasviana.dev>
2022-02-10 00:36:42 -0300

    :white_check_mark: adds theme change use case on useTheme tests

--- src/hooks/useTheme.test.ts
+++ src/hooks/useTheme.test.ts
@@ -1,13 +1,41 @@
 import {renderHook} from '@testing-library/react-hooks';
 import useTheme from './useTheme';
-import ThemeProvider from '../context/ThemeProvider';
+import ThemeProvider, {darkTheme, lightTheme} from '../context/ThemeProvider';
 import COLORS from '../constants/COLORS';
 import React from 'react';
+import {useColorScheme} from 'react-native';
+
+jest.mock('react-native', () => ({
+  useColorScheme: jest.fn(),
+}));
 
 describe('useTheme | hook | integration test', () => {
   afterEach(jest.clearAllMocks);
 
+  it('should change theme when colorScheme updates', async () => {
+    (useColorScheme as jest.Mock).mockImplementation(() => 'dark');
+
+    const {result, rerender} = renderHook(() => useTheme(), {
+      wrapper: ThemeProvider,
+    });
+
+    expect(result.current).toEqual({
+      theme: darkTheme,
+      isDark: true,
+    });
+
+    (useColorScheme as jest.Mock).mockImplementation(() => 'light');
+
+    rerender();
+
+    expect(result.current).toEqual({
+      theme: lightTheme,
+      isDark: false,
+    });
+  });
+
   it("should returns context when 'ThemeContext' is provided", () => {
+    (useColorScheme as jest.Mock).mockReturnValue('light');
     const {result} = renderHook(() => useTheme(), {wrapper: ThemeProvider});
 
     expect(result.current).toStrictEqual({
@@ -20,12 +48,17 @@ describe('useTheme | hook | integration test', () => {
   });
 
   it("should throw an error when 'ThemeContext' isn't provided", async () => {
-    jest.spyOn(React, 'useContext').mockImplementation(() => null);
+    const useContextOriginal = React.useContext;
+    const useContextSpy = jest.spyOn(React, 'useContext');
+    useContextSpy.mockImplementation(() => null);
 
     const {result} = renderHook(() => useTheme());
 
     expect(result.error).toEqual(
       Error("'useTheme' should be used within 'ThemeProvider'"),
     );
+
+    useContextSpy.mockClear();
+    React.useContext = useContextOriginal;
   });
 });
