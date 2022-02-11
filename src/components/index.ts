15e20a63b11f554e366eac20967f2eb68330ab5a
Lucas Viana <contato@lucasviana.dev>
2022-02-10 23:47:43 -0300

    :construction: creates index files to components/ and src/

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export {default as ProductItem} from './ProductItem';
+export {default as withParent} from './withParent';
+export {default as ProductList} from './ProductList';
+export {default as RoundedControlButton} from './RoundedControlButton';
+export {default as SearchBarProducts} from './SearchBarProducts';
